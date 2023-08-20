a64efbfbd3d1df10f99a0fbd9e25c3b684ed2e09
amirqeysarbeygi <amir.gheysarbeygi@gmail.com>
2023-08-20 10:15:28 +0330

    Reformat changes

--- Config/SPConfig.py
+++ Config/SPConfig.py
@@ -1,13 +1,13 @@
 sp_info_config = {
-    'database_name': 'RequestRegistrationF',
-    'schema_name': 'Merchant',
-    'table_name': 'merchant'
+    "database_name": "RequestRegistrationF",
+    "schema_name": "Merchant",
+    "table_name": "merchant",
 }
 
 sp_name_config = {
-    'Insert': 'sp_Insert',
-    'Update': 'sp_Update',
-    'VirtualDelete': 'sp_VitualDelete',
-    'Load': 'sp_Load',
-    'List': 'sp_List'
+    "Insert": "sp_Insert",
+    "Update": "sp_Update",
+    "VirtualDelete": "sp_VitualDelete",
+    "Load": "sp_Load",
+    "List": "sp_List",
 }
