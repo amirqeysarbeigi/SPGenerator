6a43e898650924f529d0a26c9f0527d626d62695
amirqeysarbeygi <amir.gheysarbeygi@gmail.com>
2023-08-20 16:48:58 +0330

    addding an optional second way for sp_virtual_delete(..) in SPBuilder

--- StoreProcedures/SPBuilder.py
+++ StoreProcedures/SPBuilder.py
@@ -115,7 +115,7 @@ def sp_delete(sp_info_config: dict, sp_name_config: str):
 
 
 def sp_virtual_delete(sp_info_config: dict, sp_name_config: str):
-    input_declaration_string = SPTools.sp_key_input_declaration_string
+    input_declaration_string = SPTools.sp_key_input_declaration_string(sp_config=sp_info_config)
     primary_keys = SPTools.primary_key_table(sp_config=sp_info_config)
     condition = SPTools.sp_conditional_selection_string(primary_keys)
 
@@ -144,3 +144,22 @@ def sp_virtual_delete(sp_info_config: dict, sp_name_config: str):
     )
 
     cursor.close()
+    
+    # * Second way to do it is to call the sp_update for this table
+    # input_for_sp_update = None
+    # cursor.execute(
+    #     f"""
+    #         CREATE PROCEDURE [{sp_info_config['schema_name']}].[{sp_name_config['VirtualDelete']}_{sp_info_config['table_name']}](
+    #             {input_declaration_string}
+    #         )
+    #         AS
+    #         BEGIN
+    #             -- SET NOCOUNT ON added to prevent extra result sets from
+    #             -- interfering with SELECT statements.
+    #             SET NOCOUNT ON;
+                
+    #             -- insert statements for procedure here
+    #             [{sp_info_config['schema_name']}].[{sp_name_config['Update']}_{sp_info_config['table_name']}] {input_for_sp_update}
+    #         END
+    #     """
+    # )
