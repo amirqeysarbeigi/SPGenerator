4539c513ecd4f3647b27cd7a2116f92cf83d5131
amirqeysarbeygi <amir.gheysarbeygi@gmail.com>
2023-08-20 15:25:21 +0330

    minor correction in sp_delete(..) from Tools.SPTools

--- StoreProcedures/SPBuilder.py
+++ StoreProcedures/SPBuilder.py
@@ -88,11 +88,8 @@ def sp_delete(sp_info_config: dict, sp_name_config: str):
         sp_config=sp_info_config
     )
     primary_keys = SPTools.primary_key_table(sp_config=sp_info_config)
-    condition = ""
-
-    for key in primary_keys[0]:
-        condition += "[" + str(key) + "]" + " = " + "@" + str(key) + "\nand "
-    condition = condition[:5]
+    
+    condition = SPTools.sp_conditional_selection_string(primary_keys)
 
     cursor = SPTools.cursor_func()
 
@@ -109,7 +106,7 @@ def sp_delete(sp_info_config: dict, sp_name_config: str):
                 
                 -- insert statements for procedure here
                 DELETE FROM [{sp_info_config['schema_name']}].[{sp_info_config['table_name']}]
-                WHERE  ({condition})
+                WHERE  {condition}
             END
         """
     )
