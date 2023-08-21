b926d1bc1893a529328044de7d76a025febbd974
amirqeysarbeygi <amir.gheysarbeygi@gmail.com>
2023-08-21 14:16:04 +0330

    added "LoadList" key to sp_name_config{..}

--- Config/SPConfig.py
+++ Config/SPConfig.py
@@ -5,10 +5,11 @@ sp_info_config = {
 }
 
 sp_name_config = {
-    "Insert": "sp_Insert",
-    "Update": "sp_Update",
-    "VirtualDelete": "sp_VirtualDelete",
-    "Delete": "sp_Delete",
-    "Load": "sp_Load",
-    "List": "sp_List",
+    "Insert": "test_sp_Insert",
+    "Update": "test_sp_Update",
+    "VirtualDelete": "test_sp_VirtualDelete",
+    "Delete": "test_sp_Delete",
+    "LoadList": "test_sp_LoadList",
+    "Load": "test_sp_Load",
+    "List": "test_sp_List",
 }
