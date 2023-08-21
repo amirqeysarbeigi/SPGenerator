e37a8212d0041de259cb799415de4f9dad67d005
amirqeysarbeygi <amir.gheysarbeygi@gmail.com>
2023-08-21 09:41:28 +0330

    sp_insert(..) minor bugfixes

--- StoreProcedures/SPBuilder.py
+++ StoreProcedures/SPBuilder.py
@@ -1,20 +1,19 @@
 from Tools import SPTools
 
-
 def sp_insert(sp_info_config: dict, sp_name_config: str):
     table_columns_raw = SPTools.sp_table_columns_info_raw(sp_info_config)
     table_columns = SPTools.sp_table_columns_info_fixed(table_columns_raw)
 
-    input_declaration_string = SPTools.sp_input_declaration_string(table_columns)
+    input_declaration_string = SPTools.sp_input_declaration_string(table_columns_fixed=table_columns)
 
-    insert_declaration_string = SPTools.sp_insert_declaration_string(table_columns)
+    insert_declaration_string = SPTools.sp_insert_declaration_string(table_columns_fixed=table_columns)
 
     insert_values_string = SPTools.sp_insert_values_string(table_columns)
 
     cursor = SPTools.cursor_func()
     cursor.execute(
         f"""  
-            CREATE PROCEDURE [{sp_info_config['schema_name']}].[{sp_name_config}_{sp_info_config['table_name']}] (
+            CREATE PROCEDURE [{sp_info_config['schema_name']}].[{sp_name_config['Insert']}_{sp_info_config['table_name']}] (
                 {input_declaration_string}
             )
             AS
